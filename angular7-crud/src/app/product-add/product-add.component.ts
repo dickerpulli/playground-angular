72f7aab677f3e15ffff0d0d072d02023a3c5a36a
Thomas Bosch <thomas.bosch@codecentric.de>
2019-02-23 00:49:26 +0100

    add product

--- angular7-crud/src/app/product-add/product-add.component.ts
+++ angular7-crud/src/app/product-add/product-add.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductAddComponent implements OnInit {
 
-  constructor() { }
+  productForm: FormGroup;
+  prod_name:string='';
+  prod_desc:string='';
+  prod_price:number=null;
+  updated_at:Date=null;
+  isLoadingResults = false;
+
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.productForm = this.formBuilder.group({
+      'prod_name' : [null, Validators.required],
+      'prod_desc' : [null, Validators.required],
+      'prod_price' : [null, Validators.required],
+      'updated_at' : [null, Validators.required]
+    });
+  }
+
+  onFormSubmit(form:NgForm) {
+    this.isLoadingResults = true;
+    this.api.addProduct(form)
+      .subscribe(res => {
+          let id = res['_id'];
+          this.isLoadingResults = false;
+          this.router.navigate(['/product-details', id]);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        });
   }
 
 }
